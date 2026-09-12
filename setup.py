0cc6bbe25b968cb680a65d66da18e0f73760688d
daviguima <dvdgmf@gmail.com>
2026-09-12 22:07:33 +0200

    chore: bump GET-Pak to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 __package__ = 'getpak'
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 
 short_description = 'Raster and vector manipulation toolbox for reproducible water quality research.'
 
